55749a5d35e2d82918bae4050d398e79423036f2
Rahul Madduluri <rahulmadduluri@gmail.com>
2015-08-26 16:13:28 -0700

    Started XCode project

---
+++ ChatApp/ChatApp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  ChatApp WatchKit Extension
+//
+//  Created by Rahul Madduluri on 8/26/15.
+//  Copyright © 2015 rahulm. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
