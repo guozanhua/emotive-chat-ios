8c3cab998d2cfa7ba0dd0fdb987855ab3fdd47a5
Spencer Congero <congero@usc.edu>
2015-08-28 18:57:00 -0700

    text fields login

--- ChatApp/ChatApp/LoginViewController.swift
+++ ChatApp/ChatApp/LoginViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class LoginViewController: UIViewController, FBSDKLoginButtonDelegate
+class LoginViewController: UIViewController, FBSDKLoginButtonDelegate, UITextFieldDelegate
 {
+    var userTextField: UITextField!
+    var passTextField: UITextField!
 
     /* UIViewController overrides */
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        //userTextField.delegate = self
         
         if (FBSDKAccessToken.currentAccessToken() != nil)
         {
@@ -26,6 +29,19 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate
             let bgColor = UIColor(red: 0, green: 239/255, blue: 224/255, alpha: 1)
             self.view.backgroundColor = bgColor
 
+            let userTextField: UITextField = UITextField(frame: CGRect(x: 80.0, y: 160.0, width: 200.0, height: 40.0))
+            self.view.addSubview(userTextField)
+            let userPlaceholder = NSAttributedString(string: "username", attributes: [NSForegroundColorAttributeName : UIColor(white: 1, alpha: 0.7)])
+            userTextField.attributedPlaceholder = userPlaceholder
+            userTextField.textColor = UIColor.whiteColor()
+            
+            let passTextField: UITextField = UITextField(frame: CGRect(x: 80.0, y: 200.0, width: 200.0, height: 40.0))
+            self.view.addSubview(passTextField)
+            let passPlaceholder = NSAttributedString(string: "password", attributes: [NSForegroundColorAttributeName : UIColor(white: 1, alpha: 0.7)])
+            passTextField.attributedPlaceholder = passPlaceholder
+            passTextField.textColor = UIColor.whiteColor()
+            passTextField.secureTextEntry = true
+
             
             let loginView : FBSDKLoginButton = FBSDKLoginButton()
             self.view.addSubview(loginView)
@@ -35,6 +51,36 @@ class LoginViewController: UIViewController, FBSDKLoginButtonDelegate
         }
 
     }
+    
+    // UITextField Delegates
+    func textFieldDidBeginEditing(textField: UITextField) {
+        print("TextField did begin editing method called")
+    }
+    func textFieldDidEndEditing(textField: UITextField) {
+        print("TextField did end editing method called")
+    }
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        print("TextField should begin editing method called")
+        return true;
+    }
+    func textFieldShouldClear(textField: UITextField) -> Bool {
+        print("TextField should clear method called")
+        return true;
+    }
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        print("TextField should snd editing method called")
+        return true;
+    }
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        print("While entering the characters this method gets called")
+        return true;
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("TextField should return method called")
+        textField.resignFirstResponder();
+        return true;
+    }
+    
     
     /* FBSDKLoginButtonDelegate methods */
     
